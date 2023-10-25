11f75d95a79d373dd5b1f02abb54670cb0761051
subzero <shubham8111994@gmail.com>
2023-10-25 15:28:34 +0530

    changing fragments

--- app/src/main/java/com/mathsgame/fragments/MainActivity.kt
+++ app/src/main/java/com/mathsgame/fragments/MainActivity.kt
@@ -2,14 +2,18 @@ package com.mathsgame.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var replace: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        replace = findViewById(R.id.buttonSecondFragment)
         val fragmentManager: FragmentManager = supportFragmentManager
         val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
 
@@ -17,5 +21,14 @@ class MainActivity : AppCompatActivity() {
 
         fragmentTransaction.add(R.id.frame, firstFragment)
         fragmentTransaction.commit()
+
+        replace.setOnClickListener {
+            val secondFragmentManager: FragmentManager = supportFragmentManager
+            val secondFragmentTransaction = secondFragmentManager.beginTransaction()
+
+            val secondFragment = SecondFragment()
+            secondFragmentTransaction.replace(R.id.frame, secondFragment)
+            secondFragmentTransaction.commit()
+        }
     }
 }
