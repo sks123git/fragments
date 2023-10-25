f4e5112a154d30cd25774d74eb466229120a81b3
subzero <shubham8111994@gmail.com>
2023-10-25 14:47:39 +0530

    Initial commit

---
+++ app/src/main/java/com/mathsgame/fragments/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.mathsgame.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+
+        val firstFragment = FirstFragment()
+
+        fragmentTransaction.add(R.id.frame, firstFragment)
+        fragmentTransaction.commit()
+    }
+}
